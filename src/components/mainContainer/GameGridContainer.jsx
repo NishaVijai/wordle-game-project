8a1647608d117ad570f9f21624e35dd667b51834
Nisha <nishavijai@gmail.com>
2025-05-15 10:23:07 +0200

    Add GameGridContainer component

--- src/components/mainContainer/GameGridContainer.jsx
+++ src/components/mainContainer/GameGridContainer.jsx
@@ -1,9 +1,20 @@
 export const GameGridContainer = () => {
+    const rows = 6;
+    const cols = 5;
+
     return (
         <div className="game-grid-container">
             <div className="game-grid">
-                {/* Game grid will be rendered here */}
-                <p>Game grid</p>
+                {[...Array(rows)].map((_, rowIndex) => (
+                    [...Array(cols)].map((_, colIndex) => (
+                        <div
+                            key={`${rowIndex}-${colIndex}`}
+                            className="box"
+                        >
+
+                        </div>
+                    ))
+                ))}
             </div>
         </div>
     )
