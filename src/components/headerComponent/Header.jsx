e9a7a6df8726f53dbee4a9f2b1784b3d7c579e1d
Nisha <nishavijai@gmail.com>
2025-05-15 14:15:23 +0200

    Update Header component with test code

--- src/components/headerComponent/Header.jsx
+++ src/components/headerComponent/Header.jsx
@@ -1,14 +1,22 @@
 export const Header = () => {
   const showHowToPlay = () => {
     const howToPlay = document.querySelector('.how-to-play');
-    howToPlay.classList.add('show');
+    howToPlay.classList.add('show-how-to-play');
     console.log('showHowToPlay');
+  }
+
+  // TODO: remove the following test code
+  const showResultContainer = () => {
+    const howToPlay = document.querySelector('.result-container');
+    howToPlay.classList.add('show-result-container');
+    console.log('showResultContainer');
   }
 
   return (
     <header>
       <h1>Play Wordle Game!</h1>
       <button className="how-to-play-button" onClick={showHowToPlay}>How To Play</button>
+      <button className="show-result-button" onClick={showResultContainer}>Show result</button>
     </header>
   )
 }
