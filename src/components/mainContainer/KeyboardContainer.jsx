c70f18c5a435fea117d5cb2aae6b5295ded289fa
Nisha <nishavijai@gmail.com>
2025-05-15 10:21:06 +0200

    Add KeyboardContainer component

--- src/components/mainContainer/KeyboardContainer.jsx
+++ src/components/mainContainer/KeyboardContainer.jsx
@@ -1,8 +1,25 @@
 export const KeyboardContainer = () => {
+    const rows = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+        ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace']
+    ];
+
     return (
         <div className="keyboard-container">
             <div className="keyboard">
-                <p>Keyboard</p>
+                {rows.map((row, rowIndex) => (
+                    <div key={rowIndex} className="keyboard-rows">
+                        {row.map((key) => (
+                            <button
+                                key={key}
+                                className="key"
+                            >
+                                {key}
+                            </button>
+                        ))}
+                    </div>
+                ))}
             </div>
         </div>
     )
