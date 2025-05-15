a9badadafb1f65fdb2021881344968d8d5ad10e7
Nisha <nishavijai@gmail.com>
2025-05-15 16:06:45 +0200

    Update ResultContainer

---
+++ src/components/result/ResultContainer.jsx
@@ -0,0 +1,38 @@
+export const ResultContainer = ({ gameWon, correctWord, onRestart }) => {
+  const closeResultContainer = () => {
+    const result = document.querySelector('.result-container');
+    result.classList.remove('show-result-container');
+    console.log('closeResultContainer');
+  };
+
+  return (
+    <div className="result-container">
+      <button className="close-show-result-button" onClick={closeResultContainer}>X</button>
+      <section className="result">
+        {gameWon ? (
+          <>
+            <h2>Congratulations!</h2>
+            <p>You guessed the word: <strong>{correctWord}</strong></p>
+            <button
+              className="restart-button"
+              onClick={onRestart}
+            >
+              Play Again
+            </button>
+          </>
+        ) : (
+          <>
+            <h2>Game Over!</h2>
+            <p>The correct word was: <strong>{correctWord}</strong></p>
+            <button
+              className="restart-button"
+              onClick={onRestart}
+            >
+              Restart Game
+            </button>
+          </>
+        )}
+      </section>
+    </div>
+  );
+};
