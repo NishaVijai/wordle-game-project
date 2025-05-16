f2bc4b3e747e8314a471da3612afd0bbc429afb9
Nisha <nishavijai@gmail.com>
2025-05-16 10:10:31 +0200

    Update Header component

--- src/components/headerComponent/Header.jsx
+++ src/components/headerComponent/Header.jsx
@@ -1,26 +1,25 @@
 export const Header = () => {
+  const input = document.querySelector('.game-grid input');
+  const howToPlay = document.querySelector('.how-to-play');
+  const resultContainer = document.querySelector('.result-container');
+
   const showHowToPlay = () => {
-    const howToPlay = document.querySelector('.how-to-play');
     howToPlay.classList.add('show-how-to-play');
-    console.log('showHowToPlay');
   }
 
   const setFocusOnInput = () => {
-    const input = document.querySelector('.game-grid input');
+    howToPlay.classList.remove('show-how-to-play');
+    resultContainer.classList.remove('show-result-container');
+
     if (input) input.focus();
-    console.log('setFocusOnInput');
   }
 
-  // TODO: remove the following test code
   const showResultContainer = () => {
-    const howToPlay = document.querySelector('.result-container');
-    howToPlay.classList.add('show-result-container');
-    console.log('showResultContainer');
+    resultContainer.classList.add('show-result-container');
   }
 
   return (
     <header>
-      {/* <h1>Play Wordle Game!</h1> */}
       <button onClick={setFocusOnInput}>Play Wordle Game</button>
       <button className="how-to-play-button" onClick={showHowToPlay}>How To Play</button>
       <button className="show-result-button" onClick={showResultContainer}>Show result</button>
