10a9db6fc340193d2529cc8a004be3b2284199eb
Nisha <nishavijai@gmail.com>
2025-05-18 19:52:11 +0200

    Update Header component

--- src/components/headerComponent/Header.jsx
+++ src/components/headerComponent/Header.jsx
@@ -1,28 +1,9 @@
-export const Header = () => {
-  const input = document.querySelector('.game-grid input');
-  const howToPlay = document.querySelector('.how-to-play');
-  const resultContainer = document.querySelector('.result-container');
-
-  const showHowToPlay = () => {
-    howToPlay.classList.add('show-how-to-play');
-  }
-
-  const setFocusOnInput = () => {
-    howToPlay.classList.remove('show-how-to-play');
-    resultContainer.classList.remove('show-result-container');
-
-    if (input) input.focus();
-  }
-
-  const showResultContainer = () => {
-    resultContainer.classList.add('show-result-container');
-  }
-
+export const Header = ({ onShowHowToPlay, onShowResult, onFocusInput }) => {
   return (
     <header>
-      <button onClick={setFocusOnInput}>Play Wordle Game</button>
-      <button className="how-to-play-button" onClick={showHowToPlay}>How To Play</button>
-      <button className="show-result-button" onClick={showResultContainer}>Show result</button>
+      <button onClick={onFocusInput}>Play Wordle Game</button>
+      <button className="how-to-play-button" onClick={onShowHowToPlay}>How To Play</button>
+      <button className="show-result-button" onClick={onShowResult}>Show result</button>
     </header>
-  )
-}
+  );
+};
