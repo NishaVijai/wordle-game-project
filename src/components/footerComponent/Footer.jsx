7980feadf0a09cd51dd4a7f7760253ebb9fcfdc5
Nisha <nishavijai@gmail.com>
2025-05-19 10:14:31 +0200

    Update Footer with GitHub repo link

--- src/components/footerComponent/Footer.jsx
+++ src/components/footerComponent/Footer.jsx
@@ -4,6 +4,7 @@ export const Footer = () => {
   return (
     <footer>
       <p>Wordle Game &copy; {currentYear} - {currentYear}</p>
+      <p>GitHub - <a href="https://github.com/NishaVijai/wordle-game-project" target="_blank" rel="noopener noreferrer" title="GitHub repository of this project will open on a new tab">Repo</a></p>
     </footer>
   )
 }
